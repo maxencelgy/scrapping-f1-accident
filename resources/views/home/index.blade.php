ebd6ad3e661b709a685831dbb28af5e38037c965
maxencelgy <maxencelgy@gmail.com>
2022-10-01 22:39:04 +0200

    cdn tailwind

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>F1 SCRAP</title>
 </head>
 <body class="bg-gray-600 scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100">
